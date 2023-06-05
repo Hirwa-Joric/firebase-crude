02ef02a0410912edc29b7d16e33d9ad66c348a9c
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-05 20:12:20 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -5,9 +5,28 @@ import { collection , getDocs} from "firebase/firestore"
 
 
 function App() {
-  const [users, setusers] = useState([]);
+  const [users, setUsers] = useState([]);
+  const userCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(userCollectionRef);
+      setUsers(data.docs.map((doc)=>({...data(),id:doc.id})))
+    }
+    getUsers()
+  }, []);
   return (
-    
+    <div className="App">
+      {users.map((user) => {
+        return (
+                  <div key={user.id}>
+            <h1>{user.name}</h1>
+            <h1>{user.age}</h1>
+            
+                  </div>
+                )
+      })}
+    </div>
   )
 }
 
