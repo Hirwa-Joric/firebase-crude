92a198bdbfdc1f4ef720f2b231bb748ea4432b95
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-05 20:12:20 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import "./App.css"
+import { db } from "./firebase"
+import { collection , getDocs} from "firebase/firestore"
+
 
 function App() {
   return (
-    <div>App</div>
+    
   )
 }
 
