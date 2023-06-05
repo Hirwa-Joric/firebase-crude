14a33d00597738a92be194335f70ce668f094e3c
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-05 20:12:20 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import { useState, useEffect } from 'react'
-import "./App.css"
-import { db } from "./firebase"
-import { collection , getDocs} from "firebase/firestore"
-
+import './App.css'
+import { db } from './firebase'
+import { collection, getDocs } from 'firebase/firestore'
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const userCollectionRef = collection(db, "user");
+  const [users, setUsers] = useState([])
+  const userCollectionRef = collection(db, 'user')
 
   useEffect(() => {
     const getUsers = async () => {
-      const data = await getDocs(userCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...data(), id: doc.id })))
-      console.log(users)
+      const data = await getDocs(userCollectionRef)
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
     }
     getUsers()
-  }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div className="App">
-      {users.map((user) => {
-        return (
-                  <div key={user.id}>
-            <h1>{user.name}</h1>
-            <h1>{user.age}</h1>
-            
-                  </div>
-                )
-      })}
+      {users.map((user) => (
+        <div key={user.id}>
+          <h1>{user.name}</h1>
+          <h1>{user.age}</h1>
+        </div>
+      ))}
     </div>
   )
 }
